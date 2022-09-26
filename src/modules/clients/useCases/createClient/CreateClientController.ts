99b9d8b8d7ca6570a4a3a14402ecbbfb0b9c21f0
gustavo-gomess <gusgegomes@hotmail.com>
2022-09-26 10:06:32 -0300

    criando rotas

--- src/modules/clients/useCases/createClient/CreateClientController.ts
+++ src/modules/clients/useCases/createClient/CreateClientController.ts
@@ -9,5 +9,7 @@ export class CreateClientController {
       username,
       password,
     });
+
+    return response.json(result);
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import express, { request, response } from "express";
+import { routes } from "./routes";
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({
-    message: "hello word",
-  });
-});
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3000, () => console.log("server runing"));
